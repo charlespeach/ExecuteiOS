9a07632a186f8789b42c4a62792da158db35157a
Charles Peach <charles@3months.com>
2014-02-22 16:17:54 +1300

    Add UIButton as a subview, that when touched hits our pressed method and logs.

--- Execute/EXEPlayViewController.m
+++ Execute/EXEPlayViewController.m
@@ -11,9 +11,24 @@
 @implementation EXEPlayViewController
 
 - (void)viewDidLoad {
-    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(20.0f, 40.0f, 280.0, 280)];
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(20.0f, 40.0f, 280.0f,44.0f )];
     label.text = @"Hello World";
+    label.textAlignment = NSTextAlignmentCenter;
+    label.textColor = [UIColor redColor];
+    // The subview is relative to its parent (self.view) so will start at 0,0
     [self.view addSubview:label];
+    
+    UIButton *button = [[UIButton alloc] initWithFrame:CGRectMake(20.0f, 60.0f, 280.0f, 440.f)];
+    [button setTitle:@"Press Me" forState:UIControlStateNormal];
+    [button setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
+    [button setTitleColor:[UIColor colorWithRed:0.0f green:0.0f blue:1.0f alpha:0.5f] forState:UIControlStateHighlighted];
+    
+    [button addTarget:self action:@selector(pressed:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:button];
+}
+
+- (void)pressed:(id)sender {
+    NSLog(@"Pressed!");
 }
 
 @end
