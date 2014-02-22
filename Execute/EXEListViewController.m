282509fa7be69c80a23ef345005bd007e7882215
Charles Peach <charles@3months.com>
2014-02-23 01:04:19 +1300

    You can now complete and uncomplete tasks. Also did some code tidy up using pragma marks.

--- Execute/EXEListViewController.m
+++ Execute/EXEListViewController.m
@@ -11,10 +11,14 @@
 @interface EXEListViewController () <UITextFieldDelegate>
 
 @property (nonatomic) NSMutableArray *tasks;
+@property (nonatomic) NSMutableArray *completedTasks;
 
 @end
 
 @implementation EXEListViewController
+
+
+#pragma mark - UIViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -30,24 +34,109 @@
     
     self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"✏️" style:UIBarButtonItemStylePlain target:self action:@selector(toggleEdit:)];
     
-    
-    NSArray *loadedTasks = [[NSUserDefaults standardUserDefaults] arrayForKey:@"tasks"];
+    NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+    NSArray *loadedTasks = [userDefaults arrayForKey:@"tasks"];
     self.tasks = [[NSMutableArray alloc] initWithArray:loadedTasks];
+    
+    loadedTasks = [userDefaults arrayForKey:@"completedTasks"];
+    self.completedTasks = [[NSMutableArray alloc] initWithArray:loadedTasks];
+}
+
+
+- (void)setEditing:(BOOL)editing animated:(BOOL)animated {
+    [super setEditing:editing animated:animated];
+    
+    if (editing) {
+        self.navigationItem.rightBarButtonItem.title = @"👍";
+    } else {
+        self.navigationItem.rightBarButtonItem.title = @"✏️";
+    }
+}
+
+
+#pragma mark - Actions
+
+- (void)toggleEdit:(id)sender {
+    [self setEditing:!self.editing animated:YES];
+}
+
+
+#pragma mark - Private
+
+- (void)save {
+    NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+    [userDefaults setObject:self.tasks forKey:@"tasks"];
+    [userDefaults setObject:self.completedTasks forKey:@"completedTasks"];
+    [userDefaults synchronize];
+}
+
+
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 2;
 }
 
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    if (section == 1) {
+        return [self.completedTasks count];
+    }
     return [self.tasks count];
 }
 
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell" forIndexPath:indexPath];
-    cell.textLabel.text = self.tasks[indexPath.row];
     
+    if (indexPath.section == 0) {
+        cell.textLabel.text = self.tasks[indexPath.row];
+        cell.textLabel.textColor = [UIColor blackColor];
+    } else {
+        cell.textLabel.text = self.completedTasks[indexPath.row];
+        cell.textLabel.textColor = [UIColor lightGrayColor];
+    }
+
     return cell;
 }
 
+
+#pragma mark - UITableViewDelegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    
+    [tableView beginUpdates];
+    
+    [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationTop];
+    
+    // Tapped an uncompleted task. Lets complete it.
+    if (indexPath.section == 0) {
+        
+        NSString *task = self.tasks[indexPath.row];
+        [self.tasks removeObjectAtIndex:indexPath.row];
+        [self.completedTasks insertObject:task atIndex:0];
+        
+        [tableView insertRowsAtIndexPaths:@[[NSIndexPath indexPathForRow:0 inSection:1]] withRowAnimation:UITableViewRowAnimationTop];
+    }
+    
+    // Tapped a completed task. Lets uncomplete it.
+    else {
+        NSString *task = self.completedTasks[indexPath.row];
+        [self.completedTasks removeObjectAtIndex:indexPath.row];
+        [self.tasks insertObject:task atIndex:0];
+        
+
+        [tableView insertRowsAtIndexPaths:@[[NSIndexPath indexPathForRow:0 inSection:0]] withRowAnimation:UITableViewRowAnimationTop];
+    }
+    
+    [tableView endUpdates];
+    
+    [self save];
+}
+
+
+#pragma mark - UITextFeildDelegate
 
 - (BOOL)textFieldShouldReturn:(UITextField *)textField {
     if ([textField.text isEqual: @""]) {
@@ -62,25 +151,8 @@
     
         textField.text = nil;
         
-        NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
-        [userDefaults setObject:self.tasks forKey:@"tasks"];
-        [userDefaults synchronize];
-    
+        [self save];
         return NO;
-    }
-}
-
-- (void)toggleEdit:(id)sender {
-    [self setEditing:!self.editing animated:YES];
-}
-
-- (void)setEditing:(BOOL)editing animated:(BOOL)animated {
-    [super setEditing:editing animated:animated];
-    
-    if (editing) {
-        self.navigationItem.rightBarButtonItem.title = @"👍";
-    } else {
-        self.navigationItem.rightBarButtonItem.title = @"✏️";
     }
 }
 
