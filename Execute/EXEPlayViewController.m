b3de4c905a7ea7a86663a0eacc5e54a8e51be7a4
Charles Peach <charles@3months.com>
2014-02-22 17:09:21 +1300

    Change the return to UIReturnKeySend and resignFirstResponder on the textField (move focus from textField and reset its text).

--- Execute/EXEPlayViewController.m
+++ Execute/EXEPlayViewController.m
@@ -14,12 +14,16 @@
     UITextField *textField = [[UITextField alloc] initWithFrame:CGRectMake(20.f, 100.0f, 280.0f, 44.0f)];
     textField.backgroundColor = [UIColor colorWithWhite:0.95f alpha:1.0f];
     textField.delegate = self;
+    textField.returnKeyType = UIReturnKeySend;
     [self.view addSubview:textField];
 }
 
 - (BOOL)textFieldShouldReturn:(UITextField *)textField {
-    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"This is the title" message:textField.text delegate:nil cancelButtonTitle:@"Alrighty" otherButtonTitles:nil];
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Message" message:textField.text delegate:nil cancelButtonTitle:@"Got it!" otherButtonTitles:nil];
     [alert show];
+    
+    [textField resignFirstResponder];
+    textField.text = nil;
     return NO;
 }
 
