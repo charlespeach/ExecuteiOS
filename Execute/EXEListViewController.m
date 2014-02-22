2c7d59f4142ccdfa4151e427cfa9b5af975a85ac
Charles Peach <charles@3months.com>
2014-02-22 20:01:26 +1300

    Register the UITableViewCell class as the class to reuse when drawing multiple cells into the UITableViewController.
    Added some logic to spit out different strings inside the cells.

--- Execute/EXEListViewController.m
+++ Execute/EXEListViewController.m
@@ -18,15 +18,27 @@
     [super viewDidLoad];
     
     self.title = @"Execute";
+    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"cell"];
 }
+
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 3;
+    return 10;
 }
 
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"cell"];
-    cell.textLabel.text = @"Say Hi!";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell" forIndexPath:indexPath];
+    
+    if (indexPath.row == 0) {
+        cell.textLabel.text = @"First";
+    } else if (indexPath.row == 1) {
+        cell.textLabel.text = @"Second";
+    } else {
+        cell.textLabel.text = @"Say Hi!";
+    }
+    
     return cell;
 }
+
 @end
